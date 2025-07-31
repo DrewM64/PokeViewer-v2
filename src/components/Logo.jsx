3766be9e92a09826a0dda94e324bb057745b712a
DrewM64 <74701517+DrewM64@users.noreply.github.com>
2025-07-31 14:13:16 -0400

    Added search and logo components

---
+++ src/components/Logo.jsx
@@ -0,0 +1,8 @@
+import logo from '../assets/Pokeviewer-logo.png'
+import './Logo.css'
+
+export function Logo() {
+    return (
+        <img src={ logo } alt="PokeViewer logo" />
+    );
+}
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,9 @@
+export function SearchBar() {
+    return (
+        <div>
+            <label>I choose you: </label>
+                <input id="pokemonName" type="text" placeholder="Pokemon Name" />
+                <button id="search">🔎</button>
+        </div>
+    )
+}
