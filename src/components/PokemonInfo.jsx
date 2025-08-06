fb493f5bdd986910a67c4185e68328f5de9cc45c
DrewM64 <74701517+DrewM64@users.noreply.github.com>
2025-08-06 16:04:25 -0400

    Add classes to HTML

--- src/components/PokemonInfo.jsx
+++ src/components/PokemonInfo.jsx
@@ -1,14 +1,14 @@
 export function PokemonInfo() {
     return (
         <div className="pokemonInfo">
-            <p>#428</p><p>Lopunny</p>
-            <p>Rabbit Pokemon</p>
-            <figure>
+            <p className="number">#428</p><p className="name">Lopunny</p>
+            <p><span className="species">Rabbit</span> Pokemon</p>
+            <figure className="cry">
                 <figcaption>Cry:</figcaption>
                 <audio controls controlsList="play" src="https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/428.ogg" />
             </figure>
-            <p>Height: 3'11"</p>
-            <p>Weight: 73.4 lbs</p>
+            <p className="height">Height: 3'11"</p>
+            <p className="weight">Weight: 73.4 lbs</p>
             <p>Type: <span className="type">Normal</span></p>
             <div className="dexInfo">
                 <p>Lopunny is constantly monitoring its surroundings. If danger approaches, this Pokémon responds with superdestructive kicks.</p>
