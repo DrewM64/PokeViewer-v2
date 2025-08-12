7c027d02e67d136637d956cbb84d1b33d1d6124a
DrewM64 <74701517+DrewM64@users.noreply.github.com>
2025-08-12 12:55:50 -0400

    Add App.jsx fetch() req for PokemonArt

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,32 @@
-// Import components (Header, Main, Nav, etc) into App.jsx, which is mounted in main.jsx 
+// Import components (Header, Main, Nav, etc) into App.jsx, which is mounted in main.jsx
+import { useState, useEffect } from "react";
 
-import './App.css'
-import { Logo } from './components/logo.jsx'
-import { SearchBar } from './components/SearchBar.jsx'
-import { PokemonArt } from './components/PokemonArt.jsx'
-import { PokemonInfo } from './components/PokemonInfo.jsx'
+import "./App.css";
+import { Logo } from "./components/logo.jsx";
+import { SearchBar } from "./components/SearchBar.jsx";
+import { PokemonArt } from "./components/PokemonArt.jsx";
+import { PokemonInfo } from "./components/PokemonInfo.jsx";
 
 function App() {
-  return (<>
-    <Logo />
-    <SearchBar />
-    <PokemonArt />
-    <PokemonInfo />
-  </>
-  )
+  const [pokemon, setPokemon] = useState([]);
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/lopunny")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setPokemon(data);
+      });
+  });
+  return (
+    <>
+      <Logo />
+      <SearchBar />
+      <PokemonArt pokemon={pokemon} />
+      <PokemonInfo />
+    </>
+  );
 }
 
-export default App
+export default App;
--- src/components/PokemonArt.jsx
+++ src/components/PokemonArt.jsx
@@ -1,10 +1,14 @@
-import './PokemonArt.css'
-import lopunny from '../assets/lopunny.png'
+import "./PokemonArt.css";
+import lopunny from "../assets/lopunny.png";
 
-export function PokemonArt() {
-    return (
-        <div className='pokemonArt'>
-            <img src={ lopunny } alt="Placeholder rabbit" />
-        </div>
-    )
+export function PokemonArt({ pokemon }) {
+  if (!pokemon) return <p>Loading...</p>;
+  return (
+    <div className="pokemonArt">
+      <img
+        src={pokemon.sprites.other["official-artwork"].front_default}
+        alt="Placeholder rabbit"
+      />
+    </div>
+  );
 }
