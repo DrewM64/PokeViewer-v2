9d6a128276ce95de9cca1682c0eb0f4ccfa68e78
DrewM64 <74701517+DrewM64@users.noreply.github.com>
2025-08-14 12:32:21 -0400

    Add fetch species data for genus & flavor text

--- src/components/PokemonInfo.jsx
+++ src/components/PokemonInfo.jsx
@@ -6,10 +6,19 @@ function capitalizeFirstLetter(string) {
 }
 
 export function PokemonInfo({ pokemon, species }) {
-  var realFeet = pokemon.height * 0.32808; //dm to decimal ft
+  var realFeet = pokemon?.height * 0.32808; //dm to decimal ft
   var feet = Math.floor(realFeet); // rounding decimal ft to whole number
   var inches = Math.round((realFeet - feet) * 12); //get inches as a whole number
-  var weightInLbs = (pokemon.weight * 0.22046).toFixed(2); //hg to lbs
+  var weightInLbs = (pokemon?.weight * 0.22046).toFixed(2); //hg to lbs
+
+  const latestFlavorTexts = species?.flavor_text_entries
+    ?.filter((entry) => entry.language.name === "en") // only English
+    .slice(-2) // last two entries
+    .map((entry, index) => (
+      <p key={index}>
+        <em>{entry.flavor_text.replace(/\f|\n|\r/g, " ").trim()}</em>
+      </p>
+    ));
 
   useEffect(() => {
     const cryAudio = document.getElementById("cryAudio");
@@ -42,10 +51,7 @@ export function PokemonInfo({ pokemon, species }) {
   return (
     <div className="pokemonInfo">
       <figure className="cry">
-        <audio
-          id="cryAudio"
-          src="https://raw.githubusercontent.com/PokeAPI/cries/main/cries/pokemon/latest/428.ogg"
-        />
+        <audio id="cryAudio" src={pokemon?.cries.latest} />
         <button id="playButton">Play cry</button>
       </figure>
       <p className="pokeId">
@@ -54,8 +60,8 @@ export function PokemonInfo({ pokemon, species }) {
           {!pokemon ? "MissingNo" : capitalizeFirstLetter(pokemon?.name)}
         </span>
       </p>
-      <p>
-        <span className="species">Rabbit Pokemon</span>
+      <p className="species">
+        {species?.genera.find((num) => num.language.name === "en").genus}{" "}
       </p>
       <div className="bioInfo">
         <p className="height">
@@ -64,18 +70,14 @@ export function PokemonInfo({ pokemon, species }) {
         <p className="weight">Weight: {weightInLbs} lbs</p>
       </div>
       <p>
-        Type: <span className="type">Normal</span>
+        Type:{" "}
+        <span className="type">
+          {pokemon?.types
+            .map((typeInfo) => capitalizeFirstLetter(typeInfo.type.name))
+            .join(", ")}
+        </span>
       </p>
-      <div className="dexInfo">
-        <p>
-          Lopunny is constantly monitoring its surroundings. If danger
-          approaches, this Pokémon responds with superdestructive kicks.
-        </p>
-        <p>
-          Once hot seasons are over, Lopunny’s coat will be replaced with fur
-          that holds a lot of insulating air in preparation for colder weather.
-        </p>
-      </div>
+      <div className="dexInfo">{latestFlavorTexts}</div>
     </div>
   );
 }
